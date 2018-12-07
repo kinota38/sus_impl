bb82aec462dce2bf8e1925524fcaf0ecc0f4bb32
kinota38 <watakino3835@gmail.com>
2018-12-07 12:37:53 +0900

    Chart.jsの追加

--- public/javascripts/sample/enquete.js
+++
@@ -1,4 +0,0 @@
-// 削除の確認ダイアログを表示する
-function confirm_delete(id) {
-    return window.confirm("#" + id + " を削除してよろしいですか");
-}
--- public/javascripts/todolist.js
+++
@@ -1,3 +0,0 @@
-function confirm_delete(id) {
-    return window.confirm("#" + id + " を削除してよろしいですか");
-}
